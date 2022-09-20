b6436ad4097e303f913c45f6984b363b8abad0df
lukendev <lucas.azevedo2104@gmail.com>
2022-09-20 15:07:48 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Gustavo Almeida', '22211133321', 8700.0, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Renã da Silva', '22246733321', 2300.0, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Julian Gonçalves', '32111133551', 5000.0, NOW(), 4);
