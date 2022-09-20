e0d119fd0d16f9396731263563836cf190beb172
lukendev <lucas.azevedo2104@gmail.com>
2022-09-20 17:50:48 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Gustavo Almeida', '22211133321', 8700.0, NOW(), 1);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Renã da Silva', '22246733321', 2300.0, NOW(), 0);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Julian Gonçalves', '32111133551', 5000.0, NOW(), 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Gustavo Almeida', '22211133321', 8700.0, NOW(), 1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Renã da Silva', '22246733321', 2300.0, NOW(), 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Julian Gonçalves', '32111133551', 5000.0, NOW(), 4, NOW());
