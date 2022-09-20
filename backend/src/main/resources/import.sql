7df9b8e58f13a30a2c399fc9e109df3cdfe2dfbc
lukendev <lucas.azevedo2104@gmail.com>
2022-09-20 19:11:59 -0300

    Changing cpfs on seeding

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Gustavo Almeida', '22211133321', 8700.0, TIMESTAMP WITH TIME ZONE '1976-01-13T20:50:07.12345Z', 1, NOW());
-INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Renã da Silva', '22246733321', 2300.0, TIMESTAMP WITH TIME ZONE '1995-04-21T20:50:07.12345Z', 0, NOW());
-INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Julian Gonçalves', '32111133551', 5000.0, TIMESTAMP WITH TIME ZONE '1965-07-11T20:50:07.12345Z', 4, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Gustavo Almeida', '85265896322', 8700.0, TIMESTAMP WITH TIME ZONE '1976-01-13T20:50:07.12345Z', 1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Renã da Silva', '89555419630', 2300.0, TIMESTAMP WITH TIME ZONE '1995-04-21T20:50:07.12345Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Julian Gonçalves', '59764414412', 5000.0, TIMESTAMP WITH TIME ZONE '1965-07-11T20:50:07.12345Z', 4, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Lucas Azevedo Benedito', '69696969641', 30000.0, TIMESTAMP WITH TIME ZONE '1996-04-21T20:50:07.12345Z', 1, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Roberto Moraes', '57597604532', 12300.0, TIMESTAMP WITH TIME ZONE '1981-12-12T20:50:07.12345Z', 0, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Alfredo Guimarães', '48653255512', 5000.0, TIMESTAMP WITH TIME ZONE '1965-07-11T20:50:07.12345Z', 4, NOW());
