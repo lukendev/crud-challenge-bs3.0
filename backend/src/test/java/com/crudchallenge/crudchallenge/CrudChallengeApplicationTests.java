0dde4b69caa5ba2239daccb53f76aa4ff29bf76b
lukendev <lucas.azevedo2104@gmail.com>
2022-09-20 11:37:31 -0300

    Backend project created

---
+++ backend/src/test/java/com/crudchallenge/crudchallenge/CrudChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudchallenge.crudchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
